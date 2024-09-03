4216c8784d01bdc08f046b44f9c1c071441023b8
JP <jp@jp>
2024-09-03 21:16:35 +0100

    Inital commit

---
+++ AgileToolbox/Hubs/PokerHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AgileToolbox.Hubs;
+
+public class PokerHub: Hub
+{
+    public async Task SendMessage(string user, string estimate)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, estimate);
+    }
+}
