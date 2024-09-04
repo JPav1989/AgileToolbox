79d7f6f6c3340d0004437eabbb6f61369bbd57fd
JP <jp@jp>
2024-09-04 15:35:11 +0100

    Added further functionality to the planning poker and made aeshetic updates

--- AgileToolbox/Hubs/PokerHub.cs
+++ AgileToolbox/Hubs/PokerHub.cs
@@ -8,4 +8,19 @@ public class PokerHub: Hub
     {
         await Clients.All.SendAsync("ReceiveMessage", user, estimate);
     }
+
+    public async Task ToggleShow(bool showEstimates)
+    {
+        await Clients.All.SendAsync("ReceiveShowMessage", showEstimates);
+    }
+
+    public async Task ClearEstimates(Dictionary<string, string> estimates)
+    {
+        await Clients.All.SendAsync("ReceiveClearEstimates", estimates);
+    }
+
+    public async Task SyncEstimates(Dictionary<string, string> estimates)
+    {
+        await Clients.All.SendAsync("ReceiveSyncEstimates", estimates);
+    }
 }
